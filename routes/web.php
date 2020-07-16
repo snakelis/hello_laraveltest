f3b4bded85c4405a9d54d1c7a21df9732c9f3594
snakelis <779248613@qq.com>
2020-07-16 19:32:16 +0800

    会员登陆

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,16 @@
 
 use Illuminate\Support\Facades\Route;
 
+//常用
 Route::get('/', 'StaticPagesController@home')->name('home');
 Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
 Route::get('signup', 'UsersController@create')->name('signup');
 
+//用户
 Route::resource('users', 'UsersController');
+
+//登陆
+Route::get('login', 'SessionsController@create')->name('login');
+Route::post('login', 'SessionsController@store')->name('login');
+Route::post('logout', 'SessionsController@destory')->name('logout');
