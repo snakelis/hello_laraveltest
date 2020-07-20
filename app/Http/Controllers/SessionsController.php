28b177d79bd4303f07bc89ce414959651613515e
snakelis <779248613@qq.com>
2020-07-20 15:17:34 +0800

    访问策略

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -9,6 +9,12 @@ use Illuminate\Support\Facades\Auth;
 class SessionsController extends Controller
 {
     //
+    public function __construct()
+    {
+        $this->middleware('guest', [
+            'only' => ['create']
+        ]);
+    }
 
     public function create()
     {
@@ -24,13 +30,15 @@ class SessionsController extends Controller
 
     public function store(Request $request)
     {
-        $this->validate($request, [
+        $credentials = $this->validate($request, [
             'email' => 'required|email|max:255',
             'password' => 'required'
         ]);
 
-        if (Auth::attempt(['email' => $request->email, 'password' => $request->password], $request->has('remember'))) {
-            return redirect()->route('users.show', [Auth::user()]);
+        if (Auth::attempt($credentials, $request->has('remember'))) {
+            session()->flash('success', '欢迎回来！');
+            $fallback = route('users.show', Auth::user());
+            return redirect()->intended($fallback);
         } else {
             session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
             return redirect()->back()->withInput();
